241ea30bc73750f3c549639e0b0dbe83b21c84ed
Asad Khan <asad.khan@lendico.de>
2021-08-24 14:40:16 +0200

    created Sort feature class

---
+++ Cypress/integration/pages/sort.js
@@ -0,0 +1,22 @@
+
+class SortFeatures
+{
+    selectSort(sortBy){
+        cy.get('#lister-sort-by-options').select(sortBy)
+    }
+
+    checkFilmList(){
+        cy.get('.lister-list').find('tr').should('have.length.greaterThan',0)
+    }
+
+    selectGenre(genre){
+        cy.get('.quickLinks').contains(genre).click()
+        cy.url().should('contain', '/search/title/?genres='+genre.toLowerCase())
+    }
+
+    checkGenreFilmList() {
+        cy.get('.lister-list').should('have.length.greaterThan', 0)
+    }
+} 
+
+export default SortFeatures;
